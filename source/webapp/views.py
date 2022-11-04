9321d4a2e7ad583feef36c3c224db1fc16bdca37
samat_sultanov <samatsayak@gmail.com>
2022-11-04 17:02:26 +0600

    создал index_view И шаблон для вывода всех тасков на основной странице; добавил url Для основной страницы

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from webapp.models import Task
+
+
+def index_view(request):
+    tasks = Task.objects.order_by("-created_at")
+    context = {"tasks": tasks}
+    return render(request, 'index.html', context)
