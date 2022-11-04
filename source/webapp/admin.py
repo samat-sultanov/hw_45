44cfa2e392b45bb7c7dfb8b2a16ec8eccd3c1b9a
samat_sultanov <samatsayak@gmail.com>
2022-11-04 16:54:19 +0600

    добавил webapp в settings.py; создал модель Task и зарегистрировал ее в админ-панели; создал и накатил миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'created_at']
+    list_display_links = ['description']
+    list_filter = ['status']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'execution_date', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Заголовок')
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0],
+                              verbose_name="Статус")
+    execution_date = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.id}. {self.title}: {self.status}: {self.execution_date}"
