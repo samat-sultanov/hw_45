f85c3b2677e3ff7028df69cd1bda46d6152978f4
samat_sultanov <samatsayak@gmail.com>
2022-11-11 00:49:02 +0600

    cоздал включаемый шаблон для формы задачи и использовал его на странице обновления задачи

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -4,6 +4,6 @@ from django.forms import widgets
 
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label='Title')
-    description = forms.TextField(max_length=200, required=False, label='Description',
+    description = forms.CharField(max_length=200, required=False, label='Description',
                                   widget=widgets.Textarea(attrs={'cols': 40, 'rows': 3}))
     execution_date = forms.DateField(required=False, label='Execution date')
